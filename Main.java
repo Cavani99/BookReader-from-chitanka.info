192a43346f9ab28053636e29da267bcad72a5fc1
Kristian <kristiian.donchev@abv.bg>
2023-12-07 15:33:54 +0200

    Initial code for Load Chapters button

--- Main.java
+++ Main.java
@@ -3,6 +3,9 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.event.ActionEvent;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 
@@ -14,6 +17,8 @@ public class Main extends JFrame {
     private JButton pickChapterButton;
     private JTextArea textArea;
     private File read;
+    private LinkedHashMap<Integer,String> chapters;
+    private HashMap<Integer,Integer>chapterStart;
 
     Main(){
         setContentPane(Panel);
@@ -39,7 +44,7 @@ public class Main extends JFrame {
             File selectedFile = fileChooser.getSelectedFile();
 
             try{
-            read=new File(selectedFile.getName());
+            read=new File(selectedFile.toURI());
             Scanner Reader=new Scanner(selectedFile);
 
             textArea.setText("FILE LOADED!!!\n");
@@ -51,6 +56,7 @@ public class Main extends JFrame {
                 if (!authorOrName){
                     if(value.trim().isEmpty()){
                         textArea.setText("Not a text file from chitanka.info");
+                        read=null;
                         break;
                     }else{
                     textArea.append("Author:"+value);
@@ -59,6 +65,7 @@ public class Main extends JFrame {
                 }else{
                     if(value.trim().isEmpty()){
                         textArea.setText("Not a text file from chitanka.info");
+                        read=null;
                     }else {
                         textArea.append("\nBook Name:" + value);
                     }
@@ -75,6 +82,62 @@ public class Main extends JFrame {
 
     public void LoadChapters(ActionEvent e) {
 
+
+        if(read!=null) {
+            try {
+                Scanner reader = new Scanner(read);
+
+                String chapterName="";
+                String currentLine;
+                int chapterStartPage=0;
+                int index=2;
+                int numberOfChapter=1;
+                chapters=new LinkedHashMap<>();
+                chapterStart=new HashMap<>();
+                for (int i = 0; i < 2; i++) {
+                    reader.nextLine();
+                    reader.nextLine();
+                }
+                while (reader.hasNextLine()){
+                    currentLine=reader.nextLine().replaceAll("\\t","");
+                    index++;
+
+                    if(currentLine.equals("") && reader.hasNextLine()){
+                        currentLine=reader.nextLine().replaceAll("\\t","");
+                        index++;
+                        if(currentLine.equals("") && reader.hasNextLine()){
+                            currentLine=reader.nextLine().replaceAll("\\t","");
+                            index++;
+                            if(!currentLine.equals("")){
+                                if(!chapterName.equals("") && chapterStartPage!=0){
+                                    if(index-chapterStartPage>=20 && Character.isUpperCase(chapterName.charAt(0))){
+                                        chapters.put(numberOfChapter,chapterName);
+                                        chapterStart.put(numberOfChapter,chapterStartPage);
+                                        numberOfChapter++;
+
+                                        chapterStartPage = index;
+                                        chapterName = currentLine;
+                                    }
+                                }else {
+                                    chapterStartPage = index;
+                                    chapterName = currentLine;
+                                }
+                            }
+                        }
+                    }
+                }
+
+
+
+                textArea.setText("");
+                for (Map.Entry<Integer,String>entry:chapters.entrySet()) {
+                    textArea.append(entry.getKey()+": "+entry.getValue().replaceAll("\\t","")+"\n");
+                }
+
+            } catch (FileNotFoundException f) {
+                f.printStackTrace();
+            }
+        }else textArea.setText("No text file opened!");
 
     }
 
